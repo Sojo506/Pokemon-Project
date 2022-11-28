2a65ad31839d855f9060fdc9a9864504d7c278ae
Sojo506 <fabiansojowork@gmail.com>
2022-11-28 03:11:23 -0600

    functions modularized

--- api/src/controllers/PokemonController.js
+++ api/src/controllers/PokemonController.js
@@ -1,62 +1,13 @@
-const { Pokemon, Type, Type_Pokemon } = require("../db");
+const { Pokemon, Type } = require("../db");
 const axios = require("axios");
-
-// ================= FUNCTIONS ===================
-// GET POKEMONS FROM THE API
-const getPokemonsApi = async () => {
-  // KEEP POKEMONES
-  const pokemons = [];
-
-  // SEARCH POKEMOS BY THE API
-  const data = await axios
-    .get("https://pokeapi.co/api/v2/pokemon?limit=40")
-    .then((response) => response.data.results) // GET INTO RESULTS ARRAY (WHERE ARE POKEMONS)
-    .then((pokemon) => pokemon);
-
-  // GET THE URL TO DO A SUBREQUEST
-  const urls = data.map((d) => axios.get(d.url)); // PENDING
-
-  // RESOLVED THEM ALREADY AND GET DATA FROM THEM
-  const pokemonsApi = await axios.all(urls).then((pokemon) => {
-    pokemon.map((p) => {
-      pokemons.push({
-        id: p.data.id,
-        name: p.data.name.charAt(0).toUpperCase() + p.data.name.slice(1),
-        hp: p.data.stats[0].base_stat,
-        attack: p.data.stats[1].base_stat,
-        defense: p.data.stats[2].base_stat,
-        speed: p.data.stats[5].base_stat,
-        height: p.data.height,
-        weight: p.data.weight,
-        image: p.data.sprites.other.dream_world.front_default,
-        types: p.data.types.map((e) => e.type.name),
-      });
-    });
-  });
-
-  return pokemons;
-};
-
-// GET POKEMONS FROM DB
-const getPokemonsDb = async () => {
-  const getter = await Pokemon.findAll({
-    include: {
-      model: Type,
-      attributes: ["name"],
-      through: {
-        attributes: [],
-      },
-    },
-  });
-  return getter;
-};
-
-const getBothPokemons = async () => {
-  const pokemonsApi = await getPokemonsApi();
-  const pokemonsDb = await getPokemonsDb();
-  const pokemons = pokemonsApi.concat(pokemonsDb);
-  return pokemons;
-};
+const {
+  getPokemonsApi,
+  getPokemonsDb,
+  getBothPokemons,
+  findPokemonDb,
+  findPokemonApi,
+  deletePokemonDb,
+} = require("../utils/PokemonUtils");
 
 // ================= HTTP METHODS ===================
 // GET /pokemons
@@ -133,60 +84,19 @@ const getPokemonsByDb = async (req, res) => {
   }
 };
 
-// GET /pokemons/:id
+// GET /pokemons/:id or name
 const getPokemonByIdName = async (req, res) => {
   const { id } = req.params;
-  if (id.length === 36) {
-    console.log("Buscando en DB");
-    const pokemonDb = await Pokemon.findOne({
-      where: {
-        id,
-      },
-      include: {
-        model: Type,
-        attributes: ["name"],
-        through: {
-          attributes: [],
-        },
-      },
-    });
-    console.log(pokemonDb)
-    return res.json([{ ...pokemonDb._previousDataValues }]);
-  }
-  
-  //if not... try searching by name
-  const pokemonDb = await Pokemon.findOne({
-    where: {
-      name: id,
-    },
-    include: {
-      model: Type,
-      attributes: ["name"],
-      through: {
-        attributes: [],
-      },
-    },
-  });
-  if(pokemonDb) return res.json([{ ...pokemonDb._previousDataValues }]);
-  
-  try {
-    console.log("Buscando en API");
-    const data = await axios
-      .get(`https://pokeapi.co/api/v2/pokemon/${id}`)
-      .then((response) => response.data);
-    const pokemon = {};
-    pokemon["id"] = data.id;
-    pokemon["name"] = data.name;
-    pokemon["hp"] = data.stats[0].base_stat;
-    pokemon["attack"] = data.stats[1].base_stat;
-    pokemon["defense"] = data.stats[2].base_stat;
-    pokemon["speed"] = data.stats[5].base_stat;
-    pokemon["height"] = data.height;
-    pokemon["weight"] = data.weight;
-    pokemon["image"] = data.sprites.other.dream_world.front_default;
-    pokemon["types"] = data.types.map((e) => e.type.name);
 
-    res.json([{ ...pokemon }]);
+  try {
+    // SEARCHING ON DATABASE
+    const pokemonDb = await findPokemonDb(id);
+    if (pokemonDb) return res.json(pokemonDb);
+
+    // SEARCHING ON API
+    const pokemonApi = await findPokemonApi(id);
+    if (pokemonApi) return res.json(pokemonApi);
+    throw new Error();
   } catch (error) {
     res.status(404).send("Not Found");
   }
