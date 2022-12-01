1066589f61454e9e314019a07ebceafb94b3f05b
Sojo506 <fabiansojowork@gmail.com>
2022-11-30 21:57:43 -0600

    Fixing Frontend

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,9 +1,8 @@
 import { applyMiddleware, legacy_createStore } from "redux";
-import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import rootReducer from "../reducer";
 
 export const store = legacy_createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  applyMiddleware(thunk)
 );
